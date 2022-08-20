c27d957b285e8b37638fb519f3e7b3e35751b6e3
Ezra-code <njerukithinji872@gmail.com>
2022-08-20 06:36:00 +0300

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  attr_accessor :opposite, :adjacent, :hypotenuse
+
+  def initialize(opposite, adjacent, hypotenuse)
+    @opposite = opposite
+    @adjacent = adjacent
+    @hypotenuse = hypotenuse
+  end
+
+  def kind 
+    if self.hypotenuse <= 0 || self.adjacent <= 0 || self.opposite <= 0
+      raise TriangleError
+    elsif self.opposite == self.hypotenuse && self.hypotenuse == self.adjacent 
+     :equilateral
+    elsif self.opposite == self.adjacent  || self.opposite == self.hypotenuse || self.hypotenuse == self.adjacent 
+      :isosceles
+    elsif self.hypotenuse != self.adjacent && self.opposite != self.hypotenuse && self.opposite != self.adjacent
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "this is not a valid triangle"
+    end
+  end
+
 end
+
+square = Triangle.new(5, 5, -2)
+
+puts square.kind
